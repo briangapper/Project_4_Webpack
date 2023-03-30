c5792e18002ff16d5cc01af4c8695a765dddd0d7
briangapper <46603246+briangapper@users.noreply.github.com>
2023-03-30 15:33:08 +0200

    Install babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$', // filtering to anything that ends in .js with RegEx
+                exclude: /none_modules/, // exclude files and directories to be loaded
+                loader: 'babel-loader' // babel-loader that allow us to use webpack on our JavaScript files
+            }
+        ]
+    }
 }
