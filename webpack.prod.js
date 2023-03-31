147738ad74e646effdc3be6cf442a80a7d30559f
briangapper <46603246+briangapper@users.noreply.github.com>
2023-03-31 18:08:48 +0200

    Add two separate webpack config files

---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$', // filtering to anything that ends in .js with RegEx
+                exclude: /none_modules/, // exclude files and directories to be loaded
+                loader: 'babel-loader' // babel-loader that allow us to use webpack on our JavaScript files
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html',
+        })
+    ]
+}
