26a43d938e3dd8dfc138e8ed1822fbe06d4e9abd
briangapper <46603246+briangapper@users.noreply.github.com>
2023-03-31 16:33:53 +0200

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -12,5 +13,10 @@ module.exports = {
             }
         ]
     },
-    mode: 'none' 
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html',
+        })
+    ]
 }
